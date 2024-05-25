17beb99586105be075534e26a9590c843d487d35
Malware <4653628+malware-dev@users.noreply.github.com>
2024-05-25 07:59:32 +0200

    Digi fixed the block spawner!

--- Source/Mdk.Extractor/Digi/TempBlockSpawn.cs
+++ Source/Mdk.Extractor/Digi/TempBlockSpawn.cs
@@ -21,118 +21,74 @@ using VRageMath;
 
 namespace Digi.BuildInfo.Features.LiveData
 {
-    public struct TempBlockSpawn
+    public class TempBlockSpawn
     {
-        public static void Spawn(MyCubeBlockDefinition def, bool deleteGridOnSpawn = true, Action<IMySlimBlock> callback = null, Vector3D? spawnPos = null)
+        public const string TempGridDisplayName = "BuildInfo_TemporaryGrid";
+
+        public static void Spawn(MyCubeBlockDefinition def, bool deleteGridOnSpawn = true, Vector3D spawnPos = default, Action<IMySlimBlock> callback = null)
         {
-            new TempBlockSpawn(def, deleteGridOnSpawn, callback, spawnPos);
+            new TempBlockSpawn(def, spawnPos, deleteGridOnSpawn, callback);
         }
 
-        readonly bool _deleteGrid;
-        readonly MyCubeBlockDefinition _blockDef;
-        readonly Action<IMySlimBlock> _callback;
+        readonly bool DeleteGrid;
+        readonly MyCubeBlockDefinition BlockDef;
+        readonly Action<IMySlimBlock> Callback;
 
-        TempBlockSpawn(MyCubeBlockDefinition def, bool deleteGridOnSpawn = true, Action<IMySlimBlock> callback = null, Vector3D? spawnAt = null)
+        TempBlockSpawn(MyCubeBlockDefinition def, Vector3D spawnPos, bool deleteGridOnSpawn = true, Action<IMySlimBlock> callback = null)
         {
-            _blockDef = def;
-            _deleteGrid = deleteGridOnSpawn;
-            _callback = callback;
+            BlockDef = def;
+            DeleteGrid = deleteGridOnSpawn;
+            Callback = callback;
 
-            MatrixD camMatrix = MyAPIGateway.Session.Camera.WorldMatrix;
-            var spawnPos = spawnAt ?? camMatrix.Translation + camMatrix.Backward * 100;
+            MyObjectBuilder_CubeBlock blockOB = (MyObjectBuilder_CubeBlock)MyObjectBuilderSerializer.CreateNewObject(def.Id);
+            blockOB.EntityId = 0;
+            blockOB.Min = Vector3I.Zero;
 
-            MyObjectBuilder_CubeBlock blockOB = CreateBlockOB(def.Id);
-
-            MyObjectBuilder_CubeGrid gridOB = new MyObjectBuilder_CubeGrid()
-            {
-                CreatePhysics = false,
-                GridSizeEnum = def.CubeSize,
-                PositionAndOrientation = new MyPositionAndOrientation(spawnPos, Vector3.Forward, Vector3.Up),
-                PersistentFlags = MyPersistentEntityFlags2.InScene,
-                IsStatic = true,
-                Editable = false,
-                DestructibleBlocks = false,
-                IsRespawnGrid = false,
-                Name = "BuildInfo_TemporaryGrid",
-            };
-
+            MyObjectBuilder_CubeGrid gridOB = MyObjectBuilderSerializer.CreateNewObject<MyObjectBuilder_CubeGrid>();
+            gridOB.EntityId = 0;
+            gridOB.DisplayName = TempGridDisplayName;
+            gridOB.CreatePhysics = false;
+            gridOB.GridSizeEnum = def.CubeSize;
+            gridOB.PositionAndOrientation = new MyPositionAndOrientation(spawnPos, Vector3.Forward, Vector3.Up);
+            gridOB.PersistentFlags = MyPersistentEntityFlags2.InScene;
+            gridOB.IsStatic = true;
+            gridOB.Editable = false;
+            gridOB.DestructibleBlocks = false;
+            gridOB.IsRespawnGrid = false;
             gridOB.CubeBlocks.Add(blockOB);
 
-            // not really required for a single grid.
-            //MyAPIGateway.Entities.RemapObjectBuilder(gridOB);
-
             MyCubeGrid grid = (MyCubeGrid)MyAPIGateway.Entities.CreateFromObjectBuilderParallel(gridOB, true, SpawnCompleted);
-            grid.DisplayName = grid.Name;
+
             grid.IsPreview = true;
             grid.Save = false;
-            grid.Flags = EntityFlags.None;
-            // grid.Render.Visible = false;
-        }
-
-        MyObjectBuilder_CubeBlock CreateBlockOB(MyDefinitionId defId)
-        {
-            MyObjectBuilder_CubeBlock blockObj = (MyObjectBuilder_CubeBlock)MyObjectBuilderSerializer.CreateNewObject(defId);
-
-            blockObj.EntityId = 0;
-            blockObj.Min = Vector3I.Zero;
-
-#if false
-            // NOTE these types do not check if their fields are null in their Remap() method.
-            MyObjectBuilder_TimerBlock timer = blockObj as MyObjectBuilder_TimerBlock;
-            if(timer != null)
-            {
-                timer.Toolbar = BuildInfoMod.Instance.Caches.EmptyToolbarOB;
-                return blockObj;
-            }
-
-            MyObjectBuilder_ButtonPanel button = blockObj as MyObjectBuilder_ButtonPanel;
-            if(button != null)
-            {
-                button.Toolbar = BuildInfoMod.Instance.Caches.EmptyToolbarOB;
-                return blockObj;
-            }
-
-            MyObjectBuilder_SensorBlock sensor = blockObj as MyObjectBuilder_SensorBlock;
-            if(sensor != null)
-            {
-                sensor.Toolbar = BuildInfoMod.Instance.Caches.EmptyToolbarOB;
-                return blockObj;
-            }
-
-            // prohibited...
-            //MyObjectBuilder_TargetDummyBlock targetDummy = blockObj as MyObjectBuilder_TargetDummyBlock;
-            //if(targetDummy != null)
-            //{
-            //    targetDummy.Toolbar = BuildInfoMod.Instance.Caches.EmptyToolbarOB;
-            //    return blockObj;
-            //}
-#endif
-
-            return blockObj;
         }
 
         void SpawnCompleted(IMyEntity ent)
         {
+            // has to be here if wanna do this, but not really important anyway
+            // if done before it fully initializes, it can crash for certain blocks, like Holo LCD
+            //ent.Render.Visible = false;
+
             IMyCubeGrid grid = ent as IMyCubeGrid;
 
             try
             {
                 IMySlimBlock block = grid?.GetCubeBlock(Vector3I.Zero);
-                if (block == null)
+                if(block == null)
                 {
-                    MyLog.Default.Error($"Can't get block from spawned entity for block: {_blockDef.Id.ToString()}; grid={grid?.EntityId.ToString() ?? "(NULL)"};)");
+                    MyLog.Default.Error($"Can't get block from spawned entity for block: {BlockDef.Id.ToString()}; grid={grid?.EntityId.ToString() ?? "(NULL)"};");
                     return;
                 }
 
-                _callback?.Invoke(block);
+                Callback?.Invoke(block);
             }
-            catch (Exception e)
+            catch(Exception e)
             {
                 MyLog.Default.Error(e.ToString());
             }
             finally
             {
-                if (_deleteGrid && grid != null)
+                if(DeleteGrid && grid != null)
                 {
                     grid.Close();
                 }
