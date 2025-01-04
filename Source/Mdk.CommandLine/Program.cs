665bf7069d1a9dee838f25a920ad8514d83fc175
Malware <4653628+malware-dev@users.noreply.github.com>
2025-01-04 08:45:32 +0100

    .

--- Source/Mdk.CommandLine/Program.cs
+++ Source/Mdk.CommandLine/Program.cs
@@ -41,7 +41,6 @@ public static class Program
     /// <returns></returns>
     public static async Task<int> Main(string[] args)
     {
-        Debugger.Launch();
         var peripherals = Peripherals.Create().FromArguments(args).Build();
         try
         {
