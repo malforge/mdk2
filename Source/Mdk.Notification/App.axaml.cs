3003becfa4735669f45c1ec8e99952e85e4294ce
Malware <4653628+malware-dev@users.noreply.github.com>
2025-01-30 18:07:17 +0100

    I _always_ forget the x64 thing... plus some project cleanup

--- Source/Mdk.Notification/App.axaml.cs
+++ Source/Mdk.Notification/App.axaml.cs
@@ -13,7 +13,7 @@ public class App : Application
 {
     public override void Initialize() => AvaloniaXamlLoader.Load(this);
 
-    public override async void OnFrameworkInitializationCompleted()
+    public override void OnFrameworkInitializationCompleted()
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
