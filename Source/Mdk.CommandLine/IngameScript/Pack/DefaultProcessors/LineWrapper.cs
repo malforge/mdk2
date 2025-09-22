98b4a5c6d397f175e8f0e9131fd38e9bb5b42dec
Malware <4653628+malware-dev@users.noreply.github.com>
2025-09-22 22:37:01 +0200

    Fixed minifier newline handling breaking when referencing minified code from preserved code.

--- Source/Mdk.CommandLine/IngameScript/Pack/DefaultProcessors/LineWrapper.cs
+++ Source/Mdk.CommandLine/IngameScript/Pack/DefaultProcessors/LineWrapper.cs
@@ -9,11 +9,11 @@ class LineWrapper() : ProgramRewriter(true)
 {
     bool _isPreservedBlock;
     LinePosition _lastNewLine;
-    
+
     public int LineWidth { get; set; } = 120;
-    
+
     void ClearLineInfo() => _lastNewLine = LinePosition.Zero;
-    
+
     int GetCharacterIndexFor(LinePosition linePosition)
     {
         var character = linePosition.Character;
@@ -21,14 +21,14 @@ class LineWrapper() : ProgramRewriter(true)
             character -= _lastNewLine.Character;
         else
             MoveToStartOfLine(linePosition);
-        
+
         return character;
     }
-    
+
     void SetLineshift(LinePosition linePosition) => _lastNewLine = linePosition;
-    
+
     void MoveToStartOfLine(LinePosition linePosition) => _lastNewLine = new LinePosition(linePosition.Line, 0);
-    
+
     public override SyntaxNode VisitInterpolatedStringExpression(InterpolatedStringExpressionSyntax node)
     {
         if (node.ShouldBePreserved())
@@ -36,13 +36,13 @@ class LineWrapper() : ProgramRewriter(true)
             if (!_isPreservedBlock)
             {
                 _isPreservedBlock = true;
-                //if (!IsAtStartOfLine())
-                //{
-                ClearLineInfo();
-                node = node.WithLeadingTrivia(node.GetLeadingTrivia().Insert(0, SyntaxFactory.EndOfLine("\n").WithAdditionalAnnotations(new SyntaxAnnotation("MDK", "preserve"))));
-                //}
+                if (!IsAtStartOfLine(node))
+                {
+                    ClearLineInfo();
+                    node = node.WithLeadingTrivia(node.GetLeadingTrivia().Insert(0, SyntaxFactory.EndOfLine("\n").WithAdditionalAnnotations(new SyntaxAnnotation("MDK", "preserve"))));
+                }
             }
-            
+
             return node;
         }
         
@@ -50,51 +50,63 @@ class LineWrapper() : ProgramRewriter(true)
         
         var span = node.GetLocation().GetLineSpan();
         var endPosition = GetCharacterIndexFor(span.EndLinePosition);
-        
+
         if (node.Span.Length < LineWidth && endPosition > LineWidth)
         {
             node = node.WithLeadingTrivia(SyntaxFactory.EndOfLine("\n").WithAdditionalAnnotations(new SyntaxAnnotation("MDK", "preserve")));
             SetLineshift(span.EndLinePosition);
         }
-        
+
         return node;
     }
-    
+
+    bool IsAtStartOfLine(SyntaxNode node)
+    {
+        // If the leading trivia of _this_ node contains a newline, or the trailing trivia of the previous token contains a newline, we are at the start of a line.
+        var leadingTrivia = node.GetLeadingTrivia();
+        if (leadingTrivia.Any(SyntaxKind.EndOfLineTrivia))
+            return true;
+        var previousToken = node.GetFirstToken().GetPreviousToken();
+        if (previousToken.TrailingTrivia.Any(SyntaxKind.EndOfLineTrivia))
+            return true;
+        return false;
+    }
+
     public override SyntaxToken VisitToken(SyntaxToken token)
     {
         token = base.VisitToken(token);
         if (token.IsKind(SyntaxKind.None))
             return token;
-        
+
         if (token.ShouldBePreserved())
         {
             if (!_isPreservedBlock)
             {
                 _isPreservedBlock = true;
-                //if (!IsAtStartOfLine())
-                //{
-                ClearLineInfo();
-                token = token.WithLeadingTrivia(token.LeadingTrivia.Insert(0, SyntaxFactory.EndOfLine("\n").WithAdditionalAnnotations(new SyntaxAnnotation("MDK", "preserve"))));
-                //}
+                if (!IsAtStartOfLine(token.Parent!))
+                {
+                    ClearLineInfo();
+                    token = token.WithLeadingTrivia(token.LeadingTrivia.Insert(0, SyntaxFactory.EndOfLine("\n").WithAdditionalAnnotations(new SyntaxAnnotation("MDK", "preserve"))));
+                }
             }
-            
+
             return token;
         }
-        
+
         _isPreservedBlock = false;
-        
+
         if (token.IsKind(SyntaxKind.None))
             return token;
-        
+
         var span = token.GetLocation().GetLineSpan();
         var endPosition = GetCharacterIndexFor(span.EndLinePosition);
-        
+
         if (token.Span.Length < LineWidth && endPosition > LineWidth)
         {
             token = token.WithLeadingTrivia(SyntaxFactory.EndOfLine("\n").WithAdditionalAnnotations(new SyntaxAnnotation("MDK", "preserve")));
             SetLineshift(span.EndLinePosition);
         }
-        
+
         return token;
     }
 }
