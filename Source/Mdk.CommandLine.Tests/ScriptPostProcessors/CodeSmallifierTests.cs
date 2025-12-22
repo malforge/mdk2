c4f1f2a064ac0395be28ce2db9ba23405f1cc19d
Michael Bisbjerg <michael@mbwarez.dk>
2025-12-22 18:02:13 +0100

    Minification step: Compact field declarations in CodeSmallifier (#102)
    * Compact field declarations in CodeSmallifier
    
    * Tests for accessibility
    
    * Simplify tests, add cases with mixed types, accessibility and values
    
    * Test grouping across field declarations
    
    * Added alternate order test for the variable merging feature (failing)
    
    * Spacings
    
    ---------
    
    Co-authored-by: Malware <4653628+malware-dev@users.noreply.github.com>

---
+++ Source/Mdk.CommandLine.Tests/ScriptPostProcessors/CodeSmallifierTests.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections.Immutable;
+using FakeItEasy;
+using Mdk.CommandLine.CommandLine;
+using Mdk.CommandLine.IngameScript.Pack;
+using Mdk.CommandLine.IngameScript.Pack.DefaultProcessors;
+using Mdk.CommandLine.Shared;
+using Mdk.CommandLine.Shared.Api;
+using Microsoft.CodeAnalysis;
+using NUnit.Framework;
+
+namespace MDK.CommandLine.Tests.ScriptPostProcessors;
+
+[TestFixture]
+public class CodeSmallifierTests : DocumentProcessorTests<CodeSmallifier>
+{
+    [Test]
+    public async Task ProcessAsync_WhenFieldsAreUninitialized_CompactsByType()
+    {
+        const string testCode =
+            """
+            class Program
+            {
+                private static string A;
+                private static string B = "x";
+                internal string C;
+                string[] D;
+                string E;
+                int F;
+                private int G = 1;
+                string H;
+            }
+            """;
+
+        var workspace = new AdhocWorkspace();
+        var project = workspace.AddProject("TestProject", LanguageNames.CSharp);
+        var document = project.AddDocument("TestDocument", testCode);
+        var processor = new CodeSmallifier();
+        var parameters = new Parameters
+        {
+            Verb = Verb.Pack,
+            PackVerb =
+            {
+                MinifierLevel = MinifierLevel.Lite,
+                ProjectFile = @"A:\Fake\Path\Project.csproj",
+                Output = @"A:\Fake\Path\Output"
+            }
+        };
+        var context = new PackContext(
+            parameters,
+            A.Fake<IConsole>(),
+            A.Fake<IInteraction>(o => o.Strict()),
+            A.Fake<IFileFilter>(o => o.Strict()),
+            A.Fake<IFileFilter>(o => o.Strict()),
+            A.Fake<IFileSystem>(),
+            A.Fake<IImmutableSet<string>>(o => o.Strict())
+        );
+
+        var result = await processor.ProcessAsync(document, context);
+        var actual = await result.GetTextAsync();
+            var expected =
+            """
+            class Program
+            {
+                private static string A,B = "x";
+                internal string C;
+                string[] D;
+                string E,H;
+                int F,G = 1;
+            }
+            """;
+
+        Assert.That(actual.ToString(), Is.EqualTo(expected));
+    }
+
+    [Test]
+    public async Task ProcessAsync_WhenFieldsAreUninitialized_CompactsByTypeSecondaryOrder()
+    {
+        const string testCode =
+            """
+            class Program
+            {
+                private static string A = "x";
+                private static string B;
+                internal string C;
+                string[] D;
+                string E;
+                int F = 1;
+                private int G;
+                string H;
+            }
+            """;
+
+        var workspace = new AdhocWorkspace();
+        var project = workspace.AddProject("TestProject", LanguageNames.CSharp);
+        var document = project.AddDocument("TestDocument", testCode);
+        var processor = new CodeSmallifier();
+        var parameters = new Parameters
+        {
+            Verb = Verb.Pack,
+            PackVerb =
+            {
+                MinifierLevel = MinifierLevel.Lite,
+                ProjectFile = @"A:\Fake\Path\Project.csproj",
+                Output = @"A:\Fake\Path\Output"
+            }
+        };
+        var context = new PackContext(
+            parameters,
+            A.Fake<IConsole>(),
+            A.Fake<IInteraction>(o => o.Strict()),
+            A.Fake<IFileFilter>(o => o.Strict()),
+            A.Fake<IFileFilter>(o => o.Strict()),
+            A.Fake<IFileSystem>(),
+            A.Fake<IImmutableSet<string>>(o => o.Strict())
+        );
+
+        var result = await processor.ProcessAsync(document, context);
+        var actual = await result.GetTextAsync();
+            var expected =
+            """
+            class Program
+            {
+                private static string A = "x",B;
+                internal string C;
+                string[] D;
+                string E,H;
+                int F = 1,G;
+            }
+            """;
+
+        Assert.That(actual.ToString(), Is.EqualTo(expected));
+    }
+}
--- Source/Mdk.CommandLine/IngameScript/Pack/DefaultProcessors/CodeSmallifier.cs
+++ Source/Mdk.CommandLine/IngameScript/Pack/DefaultProcessors/CodeSmallifier.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Mdk.CommandLine.IngameScript.Pack.Api;
@@ -43,11 +43,13 @@ public class CodeSmallifier : IDocumentProcessor
                     return node;
                 var modifierKind = _parentTypes.Count > 0 ? SyntaxKind.PrivateKeyword : SyntaxKind.InternalKeyword;
                 var modifier = newNode.Modifiers.FirstOrDefault(m => m.IsKind(modifierKind));
-                if (modifier.IsKind(SyntaxKind.None))
-                    return newNode;
+                if (!modifier.IsKind(SyntaxKind.None))
+                {
+                    newNode = newNode.WithModifiers(newNode.Modifiers.Remove(modifier))
+                        .WithLeadingTrivia(newNode.GetLeadingTrivia().AddRange(modifier.LeadingTrivia));
+                }
 
-                return newNode.WithModifiers(newNode.Modifiers.Remove(modifier))
-                    .WithLeadingTrivia(newNode.GetLeadingTrivia().AddRange(modifier.LeadingTrivia));
+                return CompactFieldDeclarations(newNode);
             }
             finally
             {
@@ -87,11 +89,13 @@ public class CodeSmallifier : IDocumentProcessor
                     return node;
                 var modifierKind = _parentTypes.Count > 0 ? SyntaxKind.PrivateKeyword : SyntaxKind.InternalKeyword;
                 var modifier = newNode.Modifiers.FirstOrDefault(m => m.IsKind(modifierKind));
-                if (modifier.IsKind(SyntaxKind.None))
-                    return newNode;
+                if (!modifier.IsKind(SyntaxKind.None))
+                {
+                    newNode = newNode.WithModifiers(newNode.Modifiers.Remove(modifier))
+                        .WithLeadingTrivia(newNode.GetLeadingTrivia().AddRange(modifier.LeadingTrivia));
+                }
 
-                return newNode.WithModifiers(newNode.Modifiers.Remove(modifier))
-                    .WithLeadingTrivia(newNode.GetLeadingTrivia().AddRange(modifier.LeadingTrivia));
+                return CompactFieldDeclarations(newNode);
             }
             finally
             {
@@ -195,6 +199,88 @@ public class CodeSmallifier : IDocumentProcessor
                 newNode = newNode.WithLeadingTrivia(newNode.GetLeadingTrivia().AddRange(modifierToRemove.LeadingTrivia));
             }
             return newNode;
+        }
+
+        static bool IsSimpleField(FieldDeclarationSyntax field)
+        {
+            if (field.AttributeLists.Count > 0)
+                return false;
+            return true;
+        }
+
+        static bool ModifiersEqual(FieldDeclarationSyntax a, FieldDeclarationSyntax b) =>
+            a.Modifiers.Select(m => m.Kind()).SequenceEqual(b.Modifiers.Select(m => m.Kind()));
+
+        static T CompactFieldDeclarations<T>(T node) where T : TypeDeclarationSyntax
+        {
+            /*
+             * Example:
+             *   // Before:
+             *   class Program
+             *   {
+             *       string A;
+             *       string[] C;
+             *       string B;
+             *       int D;
+             *       int E;
+             *   }
+             *
+             *   // After (ideal):
+             *   class Program
+             *   {
+             *       string A, B;
+             *       string[] C;
+             *       int D, E;
+             *   }
+             */
+            var members = node.Members;
+            var updated = new List<MemberDeclarationSyntax>(members.Count);
+            var i = 0;
+            while (i < members.Count)
+            {
+                if (members[i] is not FieldDeclarationSyntax field || !IsSimpleField(field))
+                {
+                    updated.Add(members[i]);
+                    i++;
+                    continue;
+                }
+
+                var segmentStart = i;
+                // Only merge contiguous simple fields to avoid reordering across other members.
+                while (i < members.Count && members[i] is FieldDeclarationSyntax nextField && IsSimpleField(nextField))
+                    i++;
+                var segmentEnd = i;
+
+                var groups = new List<(FieldDeclarationSyntax field, List<VariableDeclaratorSyntax> vars)>();
+                for (var j = segmentStart; j < segmentEnd; j++)
+                {
+                    var current = (FieldDeclarationSyntax)members[j];
+                    // Group by modifiers and type so "string" and "string[]" stay separate.
+                    var matchIndex = groups.FindIndex(g =>
+                        ModifiersEqual(g.field, current) &&
+                        g.field.Declaration.Type.IsEquivalentTo(current.Declaration.Type));
+
+                    if (matchIndex < 0)
+                        groups.Add((current, current.Declaration.Variables.ToList()));
+                    else
+                        groups[matchIndex].vars.AddRange(current.Declaration.Variables);
+                }
+
+                for (var g = 0; g < groups.Count; g++)
+                {
+                    var (seed, vars) = groups[g];
+                    var declaration = seed.Declaration.WithVariables(SyntaxFactory.SeparatedList(vars));
+                    var rebuilt = seed.WithDeclaration(declaration);
+                    // Keep leading/trailing trivia from the segment boundaries.
+                    if (g == 0)
+                        rebuilt = rebuilt.WithLeadingTrivia(members[segmentStart].GetLeadingTrivia());
+                    if (g == groups.Count - 1)
+                        rebuilt = rebuilt.WithTrailingTrivia(members[segmentEnd - 1].GetTrailingTrivia());
+                    updated.Add(rebuilt);
+                }
+            }
+
+            return (T)node.WithMembers(SyntaxFactory.List(updated));
         }
     }
 }
