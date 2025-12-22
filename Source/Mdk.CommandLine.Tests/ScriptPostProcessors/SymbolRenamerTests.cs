05d42df1cfc693a8db13e1f3af5b82229f184f9c
Michael Bisbjerg <michael@mbwarez.dk>
2025-12-22 14:57:09 +0100

    WIP on fix/foreach-var-minify

--- Source/Mdk.CommandLine.Tests/ScriptPostProcessors/SymbolRenamerTests.cs
+++ Source/Mdk.CommandLine.Tests/ScriptPostProcessors/SymbolRenamerTests.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Immutable;
+using System.Collections.Immutable;
 using FakeItEasy;
 using Mdk.CommandLine.CommandLine;
 using Mdk.CommandLine.IngameScript.Pack;
@@ -11,6 +11,121 @@ namespace MDK.CommandLine.Tests.ScriptPostProcessors;
 [TestFixture]
 public class SymbolRenamerTests : DocumentProcessorTests<SymbolRenamer>
 {
+    [Test]
+    public async Task ProcessAsync_WhenForEachUsesQualifiedType_ProducesExpectedMinifiedOutput()
+    {
+        const string testCode =
+            """
+            class Program
+            {
+                void Test()
+                {
+                    // Replacing Program.Inner with var would infer "object" and change the element type.
+                    foreach (Program.Inner item in Program.Matches())
+                    {
+                        var group = item.Groups;
+                    }
+                }
+
+                public class Inner
+                {
+                    public int Groups;
+                }
+
+                public static System.Collections.IEnumerable Matches()
+                {
+                    yield return new Inner();
+                }
+            }
+            """;
+
+        var workspace = new AdhocWorkspace();
+        var project = workspace.AddProject("TestProject", LanguageNames.CSharp);
+        var document = project.AddDocument("TestDocument", testCode);
+        var processor = new SymbolRenamer();
+        var parameters = new Parameters
+        {
+            Verb = Verb.Pack,
+            PackVerb =
+            {
+                MinifierLevel = MinifierLevel.Full,
+                ProjectFile = @"A:\Fake\Path\Project.csproj",
+                Output = @"A:\Fake\Path\Output"
+            }
+        };
+        var context = new PackContext(
+            parameters,
+            A.Fake<IConsole>(),
+            A.Fake<IInteraction>(o => o.Strict()),
+            A.Fake<IFileFilter>(o => o.Strict()),
+            A.Fake<IFileFilter>(o => o.Strict()),
+            A.Fake<IFileSystem>(),
+            A.Fake<IImmutableSet<string>>(o => o.Strict())
+        );
+
+        var result = await processor.ProcessAsync(document, context);
+        var syntaxRoot = await result.GetSyntaxRootAsync();
+        var forEach = syntaxRoot!.DescendantNodes().OfType<ForEachStatementSyntax>().Single();
+
+        Assert.That(forEach.Type, Is.TypeOf<QualifiedNameSyntax>());
+    }
+
+    [Test]
+    public async Task ProcessAsync_WhenQueryRangeVariableIsRenamed_DeclarationMatchesUsage()
+    {
+        const string testCode =
+            """
+            class Program
+            {
+                void Test()
+                {
+                    var items = new[] { "a", "b" };
+                    // "item" must be renamed consistently in both declaration and usage sites.
+                    var query =
+                        from item in items
+                        orderby item
+                        select item;
+                }
+            }
+            """;
+
+        var workspace = new AdhocWorkspace();
+        var project = workspace.AddProject("TestProject", LanguageNames.CSharp);
+        var document = project.AddDocument("TestDocument", testCode);
+        var processor = new SymbolRenamer();
+        var parameters = new Parameters
+        {
+            Verb = Verb.Pack,
+            PackVerb =
+            {
+                MinifierLevel = MinifierLevel.Full,
+                ProjectFile = @"A:\Fake\Path\Project.csproj",
+                Output = @"A:\Fake\Path\Output"
+            }
+        };
+        var context = new PackContext(
+            parameters,
+            A.Fake<IConsole>(),
+            A.Fake<IInteraction>(o => o.Strict()),
+            A.Fake<IFileFilter>(o => o.Strict()),
+            A.Fake<IFileFilter>(o => o.Strict()),
+            A.Fake<IFileSystem>(),
+            A.Fake<IImmutableSet<string>>(o => o.Strict())
+        );
+
+        var result = await processor.ProcessAsync(document, context);
+        var syntaxRoot = await result.GetSyntaxRootAsync();
+        var query = syntaxRoot!.DescendantNodes().OfType<QueryExpressionSyntax>().Single();
+        var fromIdentifier = query.FromClause.Identifier.ValueText;
+        var selectClause = (SelectClauseSyntax)query.Body.SelectOrGroup;
+        var selectIdentifier = ((IdentifierNameSyntax)selectClause.Expression).Identifier.ValueText;
+        var orderByClause = query.Body.Clauses.OfType<OrderByClauseSyntax>().Single();
+        var orderByIdentifier = ((IdentifierNameSyntax)orderByClause.Orderings.Single().Expression).Identifier.ValueText;
+
+        Assert.That(selectIdentifier, Is.EqualTo(fromIdentifier));
+        Assert.That(orderByIdentifier, Is.EqualTo(fromIdentifier));
+    }
+
 //     [Test]
 //     public async Task Regression__InheritedSymbolDidNotRename()
 //     {
