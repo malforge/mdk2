cc332a2f9e803b92cb52e228facc0e14fd4e58fc
Michael Bisbjerg <michael@mbwarez.dk>
2025-12-22 14:36:03 +0100

    Add failing test where symbol renamer is too aggressive on var replacement

--- Source/Mdk.CommandLine.Tests/ScriptPostProcessors/SymbolRenamerTests.cs
+++ Source/Mdk.CommandLine.Tests/ScriptPostProcessors/SymbolRenamerTests.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections.Immutable;
+using System.Linq;
 using FakeItEasy;
 using Mdk.CommandLine.CommandLine;
 using Mdk.CommandLine.IngameScript.Pack;
 using Mdk.CommandLine.IngameScript.Pack.DefaultProcessors;
+using Mdk.CommandLine.Shared;
+using Mdk.CommandLine.Shared.Api;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using NUnit.Framework;
 
 namespace MDK.CommandLine.Tests.ScriptPostProcessors;
@@ -11,6 +16,66 @@ namespace MDK.CommandLine.Tests.ScriptPostProcessors;
 [TestFixture]
 public class SymbolRenamerTests : DocumentProcessorTests<SymbolRenamer>
 {
+    [Test]
+    public async Task ProcessAsync_WhenForEachUsesQualifiedType_ProducesExpectedMinifiedOutput()
+    {
+        const string testCode =
+            """
+            class Program
+            {
+                void Test()
+                {
+                    // Replacing the foreach variable type with var would be incorrect here.
+                    // Var would not know the type (Program.Inner) and would use IEnumerable default (object).
+                    foreach (Program.Inner item in Program.Matches())
+                    {
+                        var group = item.Groups;
+                    }
+                }
+
+                public class Inner
+                {
+                    public int Groups;
+                }
+
+                public static System.Collections.IEnumerable Matches()
+                {
+                    yield return new Inner();
+                }
+            }
+            """;
+
+        var workspace = new AdhocWorkspace();
+        var project = workspace.AddProject("TestProject", LanguageNames.CSharp);
+        var document = project.AddDocument("TestDocument", testCode);
+        var processor = new SymbolRenamer();
+        var parameters = new Parameters
+        {
+            Verb = Verb.Pack,
+            PackVerb =
+            {
+                MinifierLevel = MinifierLevel.Full,
+                ProjectFile = @"A:\Fake\Path\Project.csproj",
+                Output = @"A:\Fake\Path\Output"
+            }
+        };
+        var context = new PackContext(
+            parameters,
+            A.Fake<IConsole>(),
+            A.Fake<IInteraction>(o => o.Strict()),
+            A.Fake<IFileFilter>(o => o.Strict()),
+            A.Fake<IFileFilter>(o => o.Strict()),
+            A.Fake<IFileSystem>(),
+            A.Fake<IImmutableSet<string>>(o => o.Strict())
+        );
+
+        var result = await processor.ProcessAsync(document, context);
+        var syntaxRoot = await result.GetSyntaxRootAsync();
+        var forEach = syntaxRoot!.DescendantNodes().OfType<ForEachStatementSyntax>().Single();
+
+        Assert.That(forEach.Type, Is.TypeOf<QualifiedNameSyntax>());
+    }
+
 //     [Test]
 //     public async Task Regression__InheritedSymbolDidNotRename()
 //     {
